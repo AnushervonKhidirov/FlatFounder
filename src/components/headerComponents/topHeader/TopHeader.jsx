94f71c8dc79161155d4bd73de5899f9465aeb3fc
AnushervonKhidirov <ankhid98@gmail.com>
2022-01-26 07:59:10 +0500

    authorized and unauthorizes user menu added

--- src/components/headerComponents/topHeader/TopHeader.jsx
+++ src/components/headerComponents/topHeader/TopHeader.jsx
@@ -1,43 +1,62 @@
 import React from 'react';
+
+import { NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { openPopup } from '../../../app/popup';
+import { signOut } from '../../../app/signIn';
+import { signInForm, signUpForm } from '../../../app/form';
+
 import Logo from '../../common/logo/Logo';
 import Button from '../../common/button/Button';
-
-import { useDispatch } from 'react-redux';
-import { openPopup } from '../../../app/popup';
-import { signInType, signUpType } from '../../../app/form';
 
 import styles from './TopHeader.module.css';
 
 
 function TopHeader() {
-    return (
-        <div className={styles.top_header}>
-            <Logo />
-            <UserMenu />
-        </div>
-    );
+    return <div className={styles.top_header}>
+        <Logo />
+        <UserMenu />
+    </div>
 }
 
-
 function UserMenu() {
+    const isSignIn = useSelector(state => state.signInHandler.value);
+
+    return <div className={styles.user_menu}>
+        {isSignIn ? <AuthorizedUserMenu /> : <UnauthorizedUserMenu />}
+    </div>
+}
+
+function AuthorizedUserMenu() {
     const dispatch = useDispatch();
 
-    function signIn() {
-        dispatch(openPopup());
-        dispatch(signInType());
+    function signOutHandler() {
+        dispatch(signOut());
     }
 
-    function signUp() {
+    return <>
+        <NavLink to='/add_offer'><Button title={'Добавить объявление'} fSize='17px' /></NavLink>
+        <Button title={'Выход'} fSize='17px' func={signOutHandler} />
+    </>
+}
+
+function UnauthorizedUserMenu() {
+    const dispatch = useDispatch();
+
+    function signInHandler() {
         dispatch(openPopup());
-        dispatch(signUpType());
+        dispatch(signInForm());
     }
 
-    return (
-        <div className={styles.user_menu}>
-            <Button title={'Вход'} fSize='17px' func={(signIn)} />
-            <Button title={'Регистрация'} fSize='17px' func={(signUp)} />
-        </div>
-    );
+    function signUpHandler() {
+        dispatch(openPopup());
+        dispatch(signUpForm());
+    }
+
+    return <>
+        <Button title={'Вход'} fSize='17px' func={(signInHandler)} />
+        <Button title={'Регистрация'} fSize='17px' func={(signUpHandler)} />
+    </>
 }
 
 export default TopHeader;
